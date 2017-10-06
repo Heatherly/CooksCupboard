aa640646d6f574f2a132a2c3af8616a28610bc1c
Michael Piatnichko <mpiatnichko@gmail.com>
2017-10-06 12:15:35 -0600

    passport and session

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,35 @@ userSchema.virtual('password')
 		this.passwordHash = hash;
 	});
 
+
+userSchema.methods.authenticate = function(password) {
+	return bcrypt.compareSync(password, this.passwordHash);
+}
+
+userSchema.statics.authenticate = function(username, password, done) {
+
+	this.findOne({ username: username }, function(err, user) {
+		if (err) {
+			console.log('error attempting to use static authenticate function', err);
+			done(err);
+		}
+		else if (user && user.authenticate(password)) {
+			console.log('this should be a good login');
+			done(null, user);
+		}
+
+		else {
+			console.log('password probably wrong');
+			done(null, false);
+		}
+
+	}); 
+}
+
+
+
+
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
